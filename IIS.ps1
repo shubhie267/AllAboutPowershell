c2381cd2eaa326b7acc1bb5add8ca749ef4e485a
Shubhi Sharma <39084244+shubhie267@users.noreply.github.com>
2021-01-11 21:10:49 +0530

    Update IIS.ps1

--- IIS.ps1
+++ IIS.ps1
@@ -1,3 +1,5 @@
-> import-module servermanager
-> add-windowsfeature web-server -includeallsubfeature
-** Powershell cmds to install IIS web server [Run command] **
+> 
+import-module servermanager
+> 
+add-windowsfeature web-server -includeallsubfeature
+**Powershell cmds to install IIS web server [Run command]**
