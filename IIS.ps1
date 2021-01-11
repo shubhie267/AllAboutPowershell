4974db056541bb1fbf6a70df9f3b5d1615e1b5cd
Shubhi Sharma <39084244+shubhie267@users.noreply.github.com>
2021-01-11 21:14:48 +0530

    Update IIS.ps1

--- IIS.ps1
+++ IIS.ps1
@@ -1,3 +1,7 @@
 import-module servermanager
 add-windowsfeature web-server -includeallsubfeature
 
+
+
+Note : Powershell cmds to install IIS web server [Run command]
+       make sure port 80 is open
